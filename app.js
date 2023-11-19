115e591638b43039beb33e19635fbcd228048ed1
hyunzzii <yhynym@gmail.com>
2023-11-19 18:12:19 +0900

    feat: router setting

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const {sequelize} = require('./models');
+const routes = require('./routes');
+
+sequelize.sync({force:false})
+.then(()=>{
+    console.log('데이터 베이스 연결에 성공하였습니다.');
+}).catch((error)=>{
+    console.log(error);
+});
+
+app.use(express.json());
+app.use('/',routes);
+
+app.listen(3000, function(){
+    console.log('Express server is listening');
+});
+
