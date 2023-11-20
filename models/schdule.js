291cfd8f3ed46d3c72b919ef81cad58f5a2ea8a9
hyunzzii <yhynym@gmail.com>
2023-11-20 10:02:19 +0900

    feat: schedule model setting

---
+++ models/schdule.js
@@ -0,0 +1,37 @@
+module.exports = (sequelize,DataTypes)=>{
+    const Schedule = sequelize.define('Schedule',{
+        title:{
+            type:DataTypes.STRING(30),
+            allowNULL:false,
+        },
+        content:{
+            type:DataTypes.STRING(1000),
+            allowNULL:true
+        },
+        date:{
+            type:DataTypes.DATEONLY,
+            allowNULL:false
+        },
+        startTime:{
+            type:DataTypes.TIME,
+            allowNULL:false
+        },
+        endTime:{
+            type:DataTypes.TIME,
+            allowNULL:false
+        }
+    },{
+        charset:'utf8',
+        collate:'utf8_general_ci',
+        tableName:'Schedule',
+        timestamps:false,
+    });
+
+    Schedule.associate = models =>{
+        Schedule.belongsTo(models.User,{
+            foreignKey:'userId',
+            onDelete: 'CASCADE'
+        })
+    }
+    return Schedule;
+};
