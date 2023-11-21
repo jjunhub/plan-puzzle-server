b9123b9b945fccb2e958484cfd8b991c1a4ac0bf
sangjun <sangjun1389@ajou.ac.kr>
2023-11-22 02:59:24 +0900

    refactor: Use errorhandler to remove try-catch at signUpController

--- controllers/singupController.js
+++ controllers/singupController.js
@@ -1,48 +1,34 @@
 const signupService = require('../services/signupService');
 
-exports.registerUser = async (req,res)=>{
-    try {
-        const userData = req.body;
-        const response = await signupService.registerUser(userData);
-        res.status(201).json(response);
-    }catch(err){
-        console.log(err);
-        res.status(500);
-    }
-}
-exports.isIdDuplicates = async(req,res)=>{
-    try{
-        const userId = req.params.id;
-        const response = await signupService.isIdDuplicates(userId);
-        res.status(200).json(response);
-    }catch(err){
-        console.log(err);
-        res.status(500);
-    }
-}
-exports.isNicknameDuplicates = async(req,res)=>{
-    try{
-        const nickname = req.params.nickname;
-        const response = await signupService.isNicknameDuplicates(nickname);
-        res.status(200).json(response);
-    }catch(err){
-        console.log(err);
-        res.status(500);
-    }
+const registerUser = async (req, res) => {
+    const userData = req.body;
+    const response = await signupService.registerUser(userData);
+
+    res.status(201).json(response);
 }
 
-exports.loginUser = async (req,res)=> {
-    try {
-        const userData = req.body;
-        const user = await signupService.loginUser(userData);
+const isIdDuplicates = async (req, res) => {
+    const userId = req.params.id;
+    const response = await signupService.isIdDuplicates(userId);
 
-        req.session.user = {
-            id: user.userId,
-        }
-
-        res.status(200).json("로그인 성공");
-    } catch(err){
-        console.log(err);
-        res.status(401)
-    }
+    res.status(200).json(response);
 }
+const isNicknameDuplicates = async (req, res) => {
+    const nickname = req.params.nickname;
+    const response = await signupService.isNicknameDuplicates(nickname);
+
+    res.status(200).json(response);
+}
+
+const loginUser = async (req, res) => {
+    const userData = req.body;
+    const user = await signupService.loginUser(userData);
+
+    req.session.user = {
+        id: user.userId,
+    }
+
+    res.status(200).json("로그인 성공");
+}
+
+module.exports = {registerUser, isIdDuplicates, isNicknameDuplicates, loginUser};
