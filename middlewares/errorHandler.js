7ed31e63d7a63a4deddc12f870e93e3bf44b780e
sangjun <sangjun1389@ajou.ac.kr>
2023-11-22 02:10:48 +0900

    feat: add wrapAsync to asynchronous error handling

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,18 +1,18 @@
 // middlewares/errorHandlers.js
+const { LoginError, NotFoundError } = require('../constants/errors');
 
-const CustomErrors = {
-    NOT_FOUND: 'NOT_FOUND',
-    INVALID_INPUT: 'INVALID_INPUT',
-};
-
-function errorHandler(err, req, res, next) {
-    if (err.message === CustomErrors.NOT_FOUND) {
-        res.status(404).send('요청한 페이지를 찾을 수 없습니다.');
-    } else if (err.message === CustomErrors.INVALID_INPUT) {
-        res.status(400).send('유효하지 않은 입력입니다.');
-    } else {
-        res.status(500).send('서버에서 오류가 발생했습니다.');
+const errorHandler = ((err, req, res, next) => {
+    if ( err.message === LoginError.MESSAGE ) {
+        res.status(LoginError.CODE).send(LoginError.MESSAGE);
+        return;
     }
-}
 
-module.exports = { CustomErrors, errorHandler };
+    if (err.message === NotFoundError.MESSAGE) {
+        res.status(404).send('요청한 페이지를 찾을 수 없습니다.');
+        return;
+    }
+
+    res.status(500).send('서버에 오류가 발생하였습니다.');
+});
+
+module.exports = { LoginError, errorHandler };
