893f1f8cbea0628bcd3ce6163d7902e49e01dab7
sangjun <sangjun1389@ajou.ac.kr>
2023-11-22 02:09:46 +0900

    feat: make constants with error information

---
+++ constants/errors.js
@@ -0,0 +1,13 @@
+
+const LoginError = {
+    MESSAGE : "해당 정보의 유저가 존재하지 않습니다.",
+    CODE : 400
+};
+
+const NotFoundError = {
+    MESSAGE : "해당 페이지를 찾을 수 없습니다.",
+    CODE : 404
+}
+
+module.exports = { LoginError, NotFoundError };
+
