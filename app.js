17dab73dd3822e409ae09368c347f588c06dbd89
sangjun <sangjun1389@ajou.ac.kr>
2023-12-08 03:16:22 +0900

    feat: Change DB setting for deploy

--- app.js
+++ app.js
@@ -1,33 +1,38 @@
-const express = require('express');
-const app = express();
-const {sequelize} = require('./models');
-const routes = require('./routes');
-const {errorHandler} = require("./middlewares/errorHandler");
-const session = require('express-session');
-const sessionConfig = require('./config/sessionConfig');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const corsConfig = require('./config/corsConfig');
-const morgan = require('morgan');
 
-dotenv.config();
+const express = require('express');
+
+const session = require('express-session');
+const cors = require('cors');
+const morgan = require('morgan');
+require('dotenv').config();
+
+const sessionConfig = require('./config/sessionConfig');
+const corsConfig = require('./config/corsConfig');
+const {sequelize} = require('./models');
+const {errorHandler} = require("./middlewares/errorHandler");
+const routes = require('./routes');
+const app = express();
+
+
 app.use(cors(corsConfig));
 app.use(session(sessionConfig));
 app.use(morgan('dev'));
 
-sequelize.sync({force:false})
-.then(()=>{
-    console.log('데이터 베이스 연결에 성공하였습니다.');
-}).catch((error)=>{
+sequelize.sync({force: false})
+    .then(() => {
+        console.log('데이터 베이스 연결에 성공하였습니다.');
+    }).catch((error) => {
     console.log(error);
+    process.exit();
 });
 
 app.use(express.json());
-app.use('/api',routes);
+app.use('/api', routes);
 
 app.use(errorHandler);
 
-app.listen(3080, function(){
-    console.log('Express server is listening');
+app.listen(process.env.PORT, function () {
+    console.log('Express server is listening at ' + process.env.PORT);
+    console.log(process.env.NODE_ENV);
 });
 
---
+++ config/DBconfig.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const env = process.env;
+
+const development = {
+    username: "plan_puzzle",
+    password: "1234",
+    database: "plan_puzzle",
+    host: "127.0.0.1",
+    dialect: "mysql"
+}
+
+const test = {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql"
+}
+
+const production = {
+  username: env.DB_USER,
+  password: env.DB_PASS,
+  database: env.DB_NAME,
+  host: env.DB_HOST,
+  dialect: "mysql"
+}
+
+module.exports = {development, test, production};
