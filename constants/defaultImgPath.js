41b3560745566a60d42b37cdc9f30849ae592b95
sangjun <sangjun1389@ajou.ac.kr>
2023-12-10 00:58:24 +0900

    fix: remove dup declaration

--- constants/defaultImgPath.js
+++ constants/defaultImgPath.js
@@ -2,6 +2,5 @@ const defaultRecruitImgPath = 'uploads/recruits/default.jpg';
 const defaultUserImgPath = 'uploads/users/default.jpg';
 const defaultChannelThumbnailPath = 'uploads/channels/default.jpg';
 const defaultChannelIconPath = 'uploads/channels/default.jpg';
-const defaultUserImgPath = 'uploads/users/default.jpg';
 
 module.exports = {defaultRecruitImgPath, defaultChannelIconPath, defaultChannelThumbnailPath, defaultUserImgPath};
